0da7deeef2680f8068b356dd917455edc2596192
ferd <beowulf1416@gmail.com>
2025-09-20 22:54:50 +0800

    initial create of workspace file

--- gtk4/oxide/src/ui/actions/open_workspace.rs
+++ gtk4/oxide/src/ui/actions/open_workspace.rs
@@ -1,5 +1,12 @@
 use log::{
-    debug
+    debug,
+    error
+};
+
+use std::io::prelude::*;
+use std::fs::{
+    self,
+    File
 };
 
 use gtk::{
@@ -34,8 +41,9 @@ pub fn open_workspace_action(
                 .pick_folder() {
                     Some(folder) => {
                         debug!("Selected folder: {:?}", folder);
+                        load_workspace(folder.to_str().unwrap());
 
-                        window.open_workspace(folder.to_str().unwrap());
+                        // window.open_workspace(folder.to_str().unwrap());
                     },
                     None => {
                         debug!("No folder selected");
@@ -44,4 +52,35 @@ pub fn open_workspace_action(
         }
     ));
     return action;
+}
+
+fn load_workspace(path: &str) {
+    debug!("Loading workspace from path: {}", path);
+
+    match fs::read_to_string(format!("{}/workspace.json", path)) {
+        Ok(content) => {
+            debug!("Workspace content: {}", content);
+        }
+        Err(e) => {
+            // error!("Failed to read workspace file: {}", e);
+            match e.kind() {
+                std::io::ErrorKind::NotFound => {
+                    debug!("Workspace file not found, creating a new one.");
+
+                    match File::create(format!("{}/workspace.json", path)) {
+                        Ok(mut file) => {
+                            let initial_content = r#"{}"#;
+                            file.write_all(initial_content.as_bytes()).unwrap();
+                        }
+                        Err(e) => {
+                            error!("Failed to create workspace file: {}", e);
+                        }
+                    }
+                }
+                _ => {
+                    error!("Failed to read workspace file: {}", e);
+                }
+            }
+        }
+    }
 }
